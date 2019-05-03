5dc94da5e498b3fef6a06677fd083bea38984885
mitu <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-05-04 02:06:00 +0600

    Create updateword.php
    Ward information update

---
+++ Ward Information Form/updateword.php
@@ -0,0 +1,48 @@
+
+<table align="center">
+<form action="updateword.php" method="post">
+<h1 align="center">Word Info Update Form</h1>
+
+      <tr>
+          <th>WARD ID</th>
+	      <td><input type="number" name="wardid" placeholder="word id" required="required"/></td>
+		  
+          <th>WARD NAME</th> 
+	      <td><input type="text" name="wardname" placeholder="word name" required="required"/></td>
+	
+	      <td colspan="2"><input type="submit" name="submit" value="Search"/></td>
+
+	  </tr>
+
+</form>
+</table>
+
+
+
+
+
+
+
+
+
+<table align="center" width="80%" border="2" style="margin-top:10px ">
+
+         <tr > 
+              <th>SERIAL NO</th>
+		      <th>REGISTRAR ID</th>
+		      <th>REGISTRAR NAME</th>
+		      <th>SERIAL NO</th>
+		      <th>BED NO</th>
+			  <th>EMPTY OR NOT EMPTY</th>
+			  <th>DELETEY</th>
+			  
+         </tr>
+		 
+		 
+		 
+		 
+		 
+
+
+
+</table>
