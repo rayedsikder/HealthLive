9171fd2a10379da6a676f9ac491bb2812e7b6eca
mitu <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-05-04 01:45:27 +0600

    Create updatemedicine.php
    Medicine enter update

---
+++ Medicine Entry Form/updatemedicine.php
@@ -0,0 +1,48 @@
+
+<table align="center">
+<form action="updatemedicine.php" method="post">
+<h1 align="center">Medicine Info Update Form</h1>
+
+       <tr>
+          <th>SUPPLIER NID</th>
+	      <td><input type="number" name="suppliernid" placeholder="NID" required="required"/></td>
+		  
+          <th>SUPPLIER NAME</th> 
+	      <td><input type="text" name="suppliername" placeholder="NAME" required="required"/></td>
+	
+	      <td colspan="2"><input type="submit" name="submit" value="Search"/></td>
+
+	  </tr>
+
+</form>
+</table>
+
+
+
+
+
+
+
+
+
+<table align="center" width="80%" border="2" style="margin-top:10px ">
+
+           <tr > 
+              <th>SERIAL NO</th>
+		      <th>MEDICINE ID</th>
+		      <th>NAME</th>
+		      <th>UNIT PRICE</th>
+		      <th>QUANTITY</th>
+			  <th>EDIT</th>
+         </tr>
+
+		 
+		 
+		 
+		 
+		 
+		 
+
+
+
+</table>
