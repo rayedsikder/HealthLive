ce0f0f7f704e404a04cd652fe9b127e9d34d75b5
mitu <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-05-04 01:47:13 +0600

    Create nurses_Information_Form.php
    Nurse information form php

---
+++ Nurses Information Form/nurses_Information_Form.php
@@ -0,0 +1,90 @@
+<html>
+
+
+
+<head>
+  <title></title>
+  <link rel="stylesheet" href="nurses_Information_Form.css" />
+ </head>
+<body>
+<div class="from_name" >
+<h1>Nurses Information Form</h1>
+
+<form action="nurses_Information_Form.php" method="post">
+
+       
+	  
+       <div class="first_name ">
+                     FIRST NAME :
+                     <p><input type="text" name="firstname" required  /></p><br/>
+	   </div>			 
+	   <div class="middle_name ">				 
+                     MIDDLE NAME :
+                     <p><input type="text" name="middlename"  required /></p><br/>
+	   </div>
+	   <div class="last_name ">				 
+                     LAST NAME:
+                     <p><input type="text"name="lastname" required /></p><br/>
+	   </div>	 
+	   <div class="date_of_admission">				 
+                     DATE OF APPOINTMENT :
+                     <p><input type="date"name="dateofadmission" required /></p><br/>
+	   </div>
+	   <div class="date_of_birth ">				 
+                     DATE OF BIRTH:
+                     <p><input type="date"name="dateofbirth" required /></p><br/>
+	   </div>
+	   <div class="phone">				 
+                     PHONE NUMBER:
+                     <p><input type="number"name="mobile" required /></p><br/>
+	   </div>
+	    <div class="word_no">				 
+                     CHOICE CABIN OR WARD NO:
+                     <p><input type="number"name="choicecabinwardno" required /></p><br/>
+	   </div>
+	   <div class="nurse_id">				 
+                     NURSES ID:
+                     <p><input type="number"name="nursesid" required /></p><br/>
+	   </div>
+	   
+	  
+	 
+       <h3>Result Information</h3>
+       <table border="2"style="width:55%; margin-left:200px; margin-top:-300px;">
+       
+       <tr>
+       <th>SSC INFORMATION</th>
+       <td><input type="text" name="ssc"  placeholder="bord  , year  ,  cgpa" required></td>
+       </tr>
+	   
+	   <tr>
+       <th>HSC INFORMATION</th>
+       <td><input type="text" name="hsc" placeholder="bord  , year  ,  cgpa" required></td>
+       </tr>
+	   
+
+	   <tr>
+       <th>BSC INFORMATION</th>
+       <td><input type="text" name="bsc"  placeholder="bord  , year  ,  cgpa" required></td>
+       </tr>
+	   
+	   <tr>
+       <th>OTHER EXAM INFORMATION</th>
+       <td><input type="text" name="other" placeholder="bord  , year  ,  cgpa" required></td>
+       </tr>
+
+	   <tr>
+       <th>ORGANIZATION</th>
+       <td><input type="text" name="organization" placeholder="organization" required></td>
+       </tr>
+
+	 
+	  <div class="submit">
+	  <p><input type="submit" name="submit"id="submit" value="submit"></p><br/>
+      </div>
+
+
+</form>
+</div>
+</body>
+</html>
