33b6a172d04cc671c21f6302fb0f734c23af3bf4
mitu <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-05-04 01:49:03 +0600

    Create updatenurses.php
    Nurse information update

---
+++ Nurses Information Form/updatenurses.php
@@ -0,0 +1,48 @@
+
+<table align="center">
+<form action="updatenurses.php" method="post">
+<h1 align="center">Nurses Info Update Form</h1>
+      <tr>
+          <th>NURSES ID</th>
+	      <td><input type="number" name="nursesid" placeholder="nurs id" required="required"/></td>
+		  
+          <th>MIDDLE NAME</th> 
+	      <td><input type="text" name="middlename" placeholder="middle name" required="required"/></td>
+	
+	      <td colspan="2"><input type="submit" name="submit" value="Search"/></td>
+
+	  </tr>
+
+</form>
+</table>
+
+
+
+
+
+
+
+
+
+<table align="center" width="80%" border="2" style="margin-top:10px ">
+
+          <tr > 
+              <th>SERIAL NO</th>
+		      <th>FIRST NAME</th>
+		      <th>MIDDLE NAME </th>
+		      <th>LAST NAME</th>
+		      <th>DATE OF BIRTHO</th>
+			  <th>PHONE NUMBER</th>
+			  <th>EDIT</th>
+			  
+         </tr>
+
+		 
+		 
+		 
+		 
+		 
+
+
+
+</table>
