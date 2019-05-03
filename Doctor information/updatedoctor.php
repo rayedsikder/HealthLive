56efc878fd0b988231d61cc194be6a04124709f6
mitu <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-05-04 02:10:11 +0600

    Create updatedoctor.php
    Doctor information updated php

---
+++ Doctor information/updatedoctor.php
@@ -0,0 +1,29 @@
+
+<table align="center">
+<form action="updatedoctor.php" method="post">
+<h1 align="center">Doctor Info Update Form</h1>
+      <tr>
+          <th>DOCTOR ID</th>
+	      <td><input type="number" name="doctorid" placeholder="doctor id" required="required"/></td>
+		  
+          <th>MIDDLE NAME</th> 
+	      <td><input type="text" name="middlename" placeholder="middle name" required="required"/></td>
+	
+	      <td colspan="2"><input type="submit" name="submit" value="Search"/></td>
+
+	  </tr>
+
+</form>
+</table>
+
+
+
+
+
+
+
+		 
+		 
+		 
+		 
+		 
